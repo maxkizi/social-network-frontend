9514834d0349ea3105fa926f7665f711ada23513
MKizilov <vtb4055428>
2022-11-25 16:25:33 +0300

    56 lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,68 +1,44 @@
-import User from "./User/User";
-import axios from "axios"
-import React from "react";
 import s from "./Users.module.css"
+import User from "./User/User";
+import React from "react";
 
-class Users extends React.Component {
+const Users = (props) => {
 
-    __mapUsers() {
+    const totalPageCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    const pageNums = []
+
+    for (let i = 1; i < totalPageCount; i++) {
+        pageNums.push(i)
+    }
+
+    const mapUsers = () => {
         return (
-            this.props.users.map(u => <User id={u.id}
-                                            name={u.name}
-                                            followed={u.followed}
-                                            status={u.status}
-                                            location={u.location}
-                                            follow={this.props.follow}
-                                            unfollow={this.props.unfollow}/>)
+            props.users.map(u => <User id={u.id}
+                                       name={u.name}
+                                       followed={u.followed}
+                                       status={u.status}
+                                       location={u.location}
+                                       follow={props.follow}
+                                       unfollow={props.unfollow}/>)
         )
     }
-
-    onPageChange = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`http://localhost:8080/api/v1/users?page=${pageNumber}&size=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setTotalUserCount(response.data.totalElements)
-                this.props.setUsers(response.data.content)
-            });
-    }
-
-
-    componentDidMount = () => {
-        axios.get(`http://localhost:8080/api/v1/users?page=${this.props.currentPage}&size=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setTotalUserCount(response.data.totalElements)
-                this.props.setUsers(response.data.content)
-            });
-    }
-
-    render = () => {
-        const totalPageCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-        const pageNums = []
-
-        for (let i = 1; i < totalPageCount; i++) {
-            pageNums.push(i)
-        }
-
-
-        return (
-            <div>
-                <div className={s.pageNumbers}>
-                    {
-                        pageNums.map(pageNumber => {
-                            return <span
-                                className={pageNumber === this.props.currentPage ? s.activePageNumber : s.pageNumber}
-                                onClick={() => {this.onPageChange(pageNumber)}}>
-                                {pageNumber} </span>
-                        })
-                    }
-                </div>
-                <div>
-                    {this.__mapUsers()}
-                </div>
+    return (
+        <div>
+            <div className={s.pageNumbers}>
+                {
+                    pageNums.map(pageNumber => {
+                        return <span
+                            className={pageNumber === props.currentPage ? s.activePageNumber : s.pageNumber}
+                            onClick={() => {
+                                props.onPageChange(pageNumber)
+                            }}>{pageNumber} </span>
+                    })
+                }
             </div>
-        )
-    }
+            <div>
+                {mapUsers()}
+            </div>
+        </div>)
 }
-
 
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,44 @@
 import {connect} from "react-redux";
-import Users from "./Users";
 import {
-    followActionCreator, setCurrentPageActionCreator,
+    followActionCreator,
+    setCurrentPageActionCreator,
     setTotalUsersCountActionCreator,
     setUsersActionCreator,
     unfollowActionCreator
 } from "../../redux/users-reducer";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
 
+
+class UsersApiComponent extends React.Component {
+
+
+    onPageChange = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`http://localhost:8080/api/v1/users?page=${pageNumber}&size=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setTotalUserCount(response.data.totalElements)
+                this.props.setUsers(response.data.content)
+            });
+    }
+
+
+    componentDidMount = () => {
+        axios.get(`http://localhost:8080/api/v1/users?page=${this.props.currentPage}&size=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setTotalUserCount(response.data.totalElements)
+                this.props.setUsers(response.data.content)
+            });
+    }
+
+    render = () => {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      onPageChange={this.onPageChange}
+                      users={this.props.users}/>
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -40,5 +72,4 @@ const mapDispatchToProps = (dispatch) => {
         }
     }
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
-export default UsersContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent)
